848c219613580c9651f1ad5b9bd9042a411e94fd
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2024-10-21 15:14:15 +0100

    Added checker class. May remove player class

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ import turn
 import win
 from random import randint
 
+class Checker:
+    def __init__(self, colour, board):
+        self.colour = colour
+        self.hit = False
+        self.beared_off = False
+        if self.colour == 'W':
+            self.home_cords = [1,6]
+            self.opp_home_cords = [0,6]
+        else:
+            self.home_cords = [0,6]
+            self.opp_home_cords[1,6]
+        self.home = board[self.home_cords[0]][self.home_cords[1]:]
+        self.opp_home = board[self.opp_home_cords[0]][self.opp_home_cords[1]:]
+        # print(self.home, self.opp_home)
+    def can_enter(self, roll):
+        valid = []
+        for die in roll:
+            if len(self.opp_home[-die][0]) < 2:
+                print('less than 2',self.opp_home[-die][0])
+                valid.append(die)
+            elif self.opp_home[-die][0][0] == self.colour:
+                print('occupied by same',self.opp_home[-die][0][0])
+                valid.append(die)
+        return valid
+
+        
+
 class Player:
     def __init__(self, colour):
         """Constructor for Player class. Sets default attribute values
@@ -36,8 +63,8 @@ class Player:
 class Board:
     def __init__(self):
         self.board = [
-                    [[['WWWWW'],[],[],[],['BBB'],[]],[['BBBBB'],[],[],[],[],['WW']]],
-                    [[['BBBBB'],[],[],[],['WWW'],[]],[['WWWWW'],[],[],[],[],['BB']]]
+                    [['WWWWW'],[],[],[],['BBB'],[],['BBBBB'],[],[],[],[],['WW']],
+                    [['BBBBB'],[],[],[],['WWW'],[],['WWWWW'],[],[],[],[],['BB']]
                     ]
     
     def show(self):
@@ -47,22 +74,18 @@ class Board:
     
 
 
-# colour_list = ['Black', 'White']
-# colourDict = {'b':"Black", 'w':'White'}
+
 inp = input("Black, White, or random? b/w/default=random").upper()
 
-# if inp in colourDict:
-#     colour = colourDict[inp]
-# else:
-#     c_index = randint(0,1)
-#     colour = colour_list[c_index]
 if inp == 'B' or inp == 'W': colour = inp
 else: colour = ['B','W'][randint(0,1)]
 
-    
+# print(len(['BBB'].pop())) 
 # Initialise player objects and board object
 player = Player(colour)
 cpu = Player([c for c in ['B','W'] if c != colour])
 board = Board()
-board.show()
+# board.show()
+c = Checker('W',board.board)
+print(c.can_enter([1,6]))
 
