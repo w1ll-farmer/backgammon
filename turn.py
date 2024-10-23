e8c5b2bdeb1b2447254864fa78ca69848252bd2c
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2024-10-23 18:34:14 +0100

    Turn plan

--- turn.py
+++ turn.py
@@ -1,4 +1,6 @@
 from random import randint
+import numpy as np
+import copy
 def roll():
     """Simulates the rolling of 2 dice
     
@@ -25,4 +27,85 @@ def is_blot(dest, opp_colour):
     
 def is_double(die1, die2):
     return die1 == die2
+
+def must_enter(board, colour):
+    if colour > 0 and board[25] > 0:
+        return False
+    elif colour < 0 and board[24] < 0:
+        return False
+    else:
+        return True
+    
+    
+def can_enter(colour, board, roll):
+    if colour == -1:
+        opp_cords = [i for i in range(0,6)]
+        opp_home = board[0:6]
+    else:
+        opp_cords = [i for i in range(23,17,-1)]
+        opp_home = board[18:24][::-1]
+    enter = []
+    for die in roll:
+        if abs(opp_home[(die)-1]) < 2:
+            enter.append(opp_cords[(die)-1])
+            # return True
+        elif opp_home[(die)-1] * colour > 0:
+            # print(opp_home[(die)-1])
+            enter.append(opp_cords[(die)-1])
+            # return True
+    return enter
+
+def turn(colour, board, roll):
+    temp_board = copy.deepcopy(board)
+    if must_enter(board, colour):
+        valid_entries = can_enter(colour, board,roll)
+        if  len(valid_entries) < 1:
+            print("No valid moves")
+            return []
+        else:
+            # Enter a piece from the bar to the board
+            print(f"Valid moves: {valid_entries}")
+            move = input("Choose move destination\n")
+            while move not in valid_entries:
+                print(f"Valid moves: {valid_entries}")
+                move = input("Choose move destination\n")
+            
+            
+            
+
+# print(can_enter(1,board = [-2,0,0,0,0,5,0,3,0,0,0,-5,5,0,0,0,-3,0,-5,0,0,0,0,2,0,0,0,0], roll=[1,2]))
+
+
+"""
+roll_dice()
+repeat getlegamoves once a move is confirmed
+getlegalmoves(colour, board, roll) calls getlegalmove(colour, board, die) for each die
+    valid = []
+    if must_enter(colour, board):
+        if can_enter(colour, board, roll):
+            valid += [bar, point]
+        return valid
+    else:
+        for point occupied by player's checker
+            if valid move?
+                valid += [start_point, end_point]
+            
+            
+valid move:
+    if all checkers home:
+        if die = exact distance to move off board, bear off
+        if furthest occupied point not as far as die roll: bear off checker on furthest point 
+        otherwise skip
+At end of game check for gammon:
+    if sum([beard off]) == 0: points = 2
+backgammon too
+
+also give opportunity for doubling cube throughout: implement after core engine
+    """
+    
+    
+    
+    
+    
+
 
