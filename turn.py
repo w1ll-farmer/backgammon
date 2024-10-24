6d8664d3252b1543913c324e1d3c0c8cbf4ad0dd
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2024-10-24 13:24:31 +0100

    Added image to remember which way round pieces go

--- turn.py
+++ turn.py
@@ -28,6 +28,9 @@ def is_blot(dest, opp_colour):
 def is_double(die1, die2):
     return die1 == die2
 
+def update_board(board, move):
+    pass
+
 def must_enter(board, colour):
     if colour > 0 and board[25] > 0:
         return False
@@ -37,7 +40,7 @@ def must_enter(board, colour):
         return True
     
     
-def can_enter(colour, board, roll):
+def can_enter(colour, board, die):
     if colour == -1:
         opp_cords = [i for i in range(0,6)]
         opp_home = board[0:6]
@@ -45,15 +48,48 @@ def can_enter(colour, board, roll):
         opp_cords = [i for i in range(23,17,-1)]
         opp_home = board[18:24][::-1]
     enter = []
-    for die in roll:
-        if abs(opp_home[(die)-1]) < 2:
-            enter.append(opp_cords[(die)-1])
-            # return True
-        elif opp_home[(die)-1] * colour > 0:
-            # print(opp_home[(die)-1])
-            enter.append(opp_cords[(die)-1])
-            # return True
-    return enter
+    if abs(opp_home[(die)-1]) < 2:
+        enter.append(opp_cords[(die)-1])
+        # return True
+    elif opp_home[(die)-1] * colour > 0:
+        # print(opp_home[(die)-1])
+        enter.append(opp_cords[(die)-1])
+        # return True
+    return (24.5+(colour/2),enter)
+
+def get_valid_moves(colour, board, roll):
+    moves = []
+    boards = []
+
+def get_legal_move(colour, board, die):
+    valid_moves = []
+    if must_enter(board, colour):
+        move = can_enter(colour, board, die)
+        if move:
+            valid_moves.append(move)
+    else:
+        if colour == -1:
+            possible_starts = [i for i in range(0,24) if board[i] < 0]
+            for p in possible_starts:
+                if board[p+die] < 2:
+                    valid_moves.append((p, p+die))
+        else:
+            possible_starts = [i for i in range(0,24) if board[i] > 0]
+            for p in possible_starts:
+                if board[p-die] > 2:
+                    valid_moves.append((p, p-die))
+
+def can_bear_off(colour, board):
+    pass
+
+def game_over(board):
+    return board[24] == 15 or board[25] == -15
+
+def is_gammon(board):
+    pass
+
+def is_backgammon(board):
+    pass
 
 def turn(colour, board, roll):
     temp_board = copy.deepcopy(board)
@@ -74,7 +110,7 @@ def turn(colour, board, roll):
             
 
 # print(can_enter(1,board = [-2,0,0,0,0,5,0,3,0,0,0,-5,5,0,0,0,-3,0,-5,0,0,0,0,2,0,0,0,0], roll=[1,2]))
-
+print(game_over([-2,0,0,0,0,5,0,3,0,0,0,-5,5,0,0,0,-3,0,-5,0,0,0,0,2,0,0,0,0]))
 
 """
 roll_dice()
