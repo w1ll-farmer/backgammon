cb25c3314d0e4210c04656ef0758bb1e56126d8b
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2024-11-12 13:52:01 +0000

    Refactored main loop into functions for easier GUI implementation

--- Code/main.py
+++ Code/main.py
@@ -4,6 +4,39 @@ from turn import *
 import numpy as np
 from time import sleep 
 
+def start_turn(player, board):
+    roll = roll_dice()
+    print(f"Player {player} rolled {roll}")
+    moves, boards = get_valid_moves(player, board, roll)
+    return moves, boards, roll
+
+def human_play(moves, boards):
+    moves_stringified = [str(move1) for move1 in moves]
+    move = input("Enter move.")
+    while move not in moves_stringified:
+        print("Enter a valid move")
+        move = input("")
+    move_index = moves_stringified.index(move)
+    board = boards[move_index]
+    return board
+
+def randobot_play(roll, moves, boards):
+    move = []
+    attempts = 0
+    
+    while move not in moves and attempts < 200000:
+        move = []
+        for _ in range(1+is_double(roll)):
+            move.append(generate_random_move())
+            move.append(generate_random_move())
+        attempts += 1
+        
+    if move not in moves:
+        move = moves[randint(0, len(moves))]
+        
+    board = boards[moves.index(move)]
+    return board, move
+
 # Each player rolls a die to determine who moves first
 black_roll, white_roll = roll_dice()
 # Loops in scenario rolls are equal
@@ -31,23 +64,19 @@ while not game_over(board):
         # Initial roll made up of both starting dice
         roll = [black_roll, white_roll]
         moves1, boards1 = get_valid_moves(player1, board, roll)
+        print_board(board)
+        print(f"Player {player1} rolled {roll}")
     else:
         # All other rolls are generated on spot
-        roll = roll_dice()
-        moves1, boards1 = get_valid_moves(player1, board, roll)
-        
+        moves1, boards1, roll = start_turn(player1, board)
+    
+    sleep(0.5)
     if player1 == 1:
-        print(f"Player {player1} rolled {roll}")
-        moves1_stringified = [str(move1) for move1 in moves1]
-        move = input("Enter move.")
-        while move not in moves1_stringified:
-            print("Enter a valid move")
-            move = input("")
-        # boards1_stringified = [str(board) for board in boards1]
-        # print(boards1_stringified)
-        move_index = moves1_stringified.index(move)
-        board = boards1[move_index]
+        
+        board = human_play(moves1, boards1)
         print_board(board)
+        
+        # Game ends?
         if is_error(board):
             sleep(2)
             break
@@ -58,44 +87,12 @@ while not game_over(board):
         
         # Player 2's turn
         
-        
-        roll = roll_dice()
-        print(f"Player {player2} rolled {roll}")
-        moves2, boards2 = get_valid_moves(player2, board, roll)
-        move = []
-        attempts = 0
-        
-        while move not in moves2 and attempts < 200000:
-            move = []
-            for i in range(1+is_double(roll)):
-                move.append(generate_random_move())
-                move.append(generate_random_move())
-            attempts += 1
-            
-        if move not in moves2:
-            move = moves2[randint(0, len(moves2))]
-            
+        moves2, boards2, roll = start_turn(player2, board)
+        board, move = randobot_play(roll, moves2, boards2)
         print(f"Move Taken: {move}")
-        board = boards2[moves2.index(move)]
-        
     else:
-        # If AI is player 1
-        print(f"Player {player1} rolled {roll}")
-        moves1, boards1 = get_valid_moves(player1, board, roll)
-        move = []
-        attempts = 0
-        while move not in moves1 and attempts < 200000:
-            move = []
-            for i in range(1+is_double(roll)):
-                move.append(generate_random_move())
-                move.append(generate_random_move())
-            attempts +=1
-            
-        if move not in moves2:
-            move = moves1[randint(0, len(moves1))]
-            
+        board, move = randobot_play(roll, moves1, boards1)
         print(f"Move Taken: {move}")
-        board = boards1[moves1.index(move)]
         print_board(board)
         sleep(1)
         if is_error(board):
@@ -104,18 +101,8 @@ while not game_over(board):
         if game_over(board):
             break
         # Player 2 turn
-        roll = roll_dice()
-        print(f"Player {player2} rolled {roll}")
-        moves2, boards2 = get_valid_moves(player2, board, roll)
-        moves2_stringified = [str(move2) for move2 in moves2]
-        move = input("Enter move.")
-        while move not in moves2_stringified:
-            print("Enter a valid move")
-            move = input("")
-        # boards1_stringified = [str(board) for board in boards1]
-        # print(boards1_stringified)
-        move_index = moves2_stringified.index(move)
-        board = boards2[move_index]
+        moves2, boards2, roll = start_turn(player2, board)
+        board = human_play(moves2, boards2)
     print_board(board)
     if is_error(board):
         sleep(2)
