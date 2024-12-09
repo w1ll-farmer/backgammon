943abf8e2fd5b04058257f1269d7725986ceb22c
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2024-12-09 12:00:54 +0000

    Final push before presentation

--- Code/data.py
+++ Code/data.py
@@ -2,3 +2,28 @@ def log(board_before,roll,  move, board_after):
     myFile = open("./Data/log.txt",'a')
     myFile.write(f"{board_before}\t{roll}\t{move}\t{board_after}\n")
     myFile.close()
+
+def greedy_summarise():
+    myFile = open("./Data/greedydata.txt")
+    white_score = 0
+    black_score = 0
+    times_won = 0
+    games_played = 0
+    for line in myFile:
+        line = line.strip("(")
+        line = line.strip(")")
+        white_points, black_points = line.split(",")
+        black_points = int(black_points.strip('\n'))
+        white_points = int(white_points)
+        if white_points - black_points > 0:
+            times_won +=1
+        white_score += white_points
+        black_score += 1
+        games_played += 1
+    return white_score, black_score, games_played, times_won
+
+def timestep(timestep):
+    myFile = open("./Data/randomvrandom.txt",'a')
+    myFile.write(f"{timestep}\n")
+    myFile.close()
+    
---
+++ Code/visualize.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import seaborn as sns  
+import matplotlib.pyplot as plt
+# from data import *
+
+
+# total_white_score, total_black_score, games_played, times_won = greedy_summarise()
+
+myFile = open("./Data/greedydata.txt")
+white_score = []
+black_score = []
+for line in myFile:
+    line = line.strip("(")
+    line = line.strip(")")
+    line = line.strip('\n')
+    line = line.strip(")")
+    white_points, black_points = line.split(",")
+    black_points = int(black_points)
+    white_points = int(white_points)
+    if len(white_score) > 0:
+        white_score.append(white_points+white_score[-1])
+        black_score.append(black_points+black_score[-1])
+    else:
+        white_score.append(white_points)
+        black_score.append(black_points)
+
+games = list(range(1, len(white_score) + 1))  # Game numbers (1-based)
+
+# Create scatter plot
+plt.figure(figsize=(8, 6))
+plt.plot(games, white_score, color='blue', label='Greedy Agent', linewidth=3)
+plt.plot(games, black_score, color='red', label='Random Agent', linewidth=3)
+
+# Add labels, legend, and title
+plt.xlabel('Game Number')
+plt.ylabel('Cumulative Score')
+plt.title('Cumulative Scores across 98 First-to-Five Matches')
+plt.legend()
+plt.grid(True)
+plt.show()
