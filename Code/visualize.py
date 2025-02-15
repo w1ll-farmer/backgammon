d876766e5530a5d22b28e0974b175fc8e311b78a
w1ll-farmer <90869013+w1ll-farmer@users.noreply.github.com>
2025-02-15 13:02:24 +0000

    Updated visualiser

--- Code/visualize.py
+++ Code/visualize.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 myFile = open("./Data/geneticvsgreedy.txt")
 white_score = []
 black_score = []
+white_wins = []
+black_wins = []
 for line in myFile:
     line = line.strip("(")
     line = line.strip(")")
@@ -24,18 +26,34 @@ for line in myFile:
     else:
         white_score.append(white_points)
         black_score.append(black_points)
+    if white_points > black_points:
+        if len(white_wins) > 0:
+            white_wins.append(white_wins[-1]+1)
+            black_wins.append(black_wins[-1])
+        else:
+            white_wins.append(1)
+            black_wins.append(0)
+    else:
+        if len(white_wins) > 0:
+            white_wins.append(white_wins[-1])
+            black_wins.append(black_wins[-1]+1)
+        else:
+            white_wins.append(0)
+            black_wins.append(1)
+        
 
 games = list(range(1, len(white_score) + 1))  # Game numbers (1-based)
 
 # Create scatter plot
 plt.figure(figsize=(8, 6))
-plt.plot(games, white_score, color='blue', label='Genetic Agent', linewidth=3)
-plt.plot(games, black_score, color='red', label='Greedy Agent', linewidth=3)
-
+plt.plot(games, white_score, color='blue', label='Genetic Score', linewidth=3)
+plt.plot(games, black_score, color='red', label='Greedy Score', linewidth=3)
+plt.plot(games, white_wins, color='purple', label='Genetic Wins', linewidth=3)
+plt.plot(games, black_wins, color='orange', label='Greedy Wins', linewidth=3)
 # Add labels, legend, and title
 plt.xlabel('Game Number')
-plt.ylabel('Cumulative Score')
-plt.title('Cumulative Scores Across 10000 Games')
+plt.ylabel('Cumulative Totals')
+plt.title('Cumulative Scores and Wins Across 10000 Games')
 plt.legend()
 plt.grid(True)
 plt.show()
